0589a110788f38a0ccaf57d432f67bdc3264d842
JavierPA3 <91657168+JavierPA3@users.noreply.github.com>
2023-04-23 21:47:21 +0200

    Update movie_api_test.py

--- project/movie_api_test.py
+++ project/movie_api_test.py
@@ -17,15 +17,15 @@ while True:
 
     match menu.choose_option():
         case 1:
-            MovieApi.show_movie_id_by_name()
+            movie_api.show_movie_id_by_name()
         case 2:
-            MovieApi.return_movie_details()
+            movie_api.return_movie_details()
         case 3:
-            MovieApi.return_recommended_movies()
+            movie_api.return_recommended_movies()
         case 4:
-            MovieApi.print_five_trending_films()
+            movie_api.print_five_trending_films()
         case 5:
-            MovieApi.list_all_movies_genres()
+            movie_api.list_all_movies_genres()
         case 6:
             print("Programa cerrado.")
             break
