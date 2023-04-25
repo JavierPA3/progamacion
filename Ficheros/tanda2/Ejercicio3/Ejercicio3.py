e5407f7558dc50b5d942914a5b6f78354e037dc7
JavierPA3 <91657168+JavierPA3@users.noreply.github.com>
2023-04-25 21:01:46 +0200

    Update Ejercicio3.py

--- Ficheros/tanda2/Ejercicio3/Ejercicio3.py
+++ Ficheros/tanda2/Ejercicio3/Ejercicio3.py
@@ -1,7 +1,8 @@
 """
 3. Realiza un programa que diga cuántas ocurrencias de una palabra hay en un fichero.
 Tanto el nombre del fichero como la palabra se deben pasar como argumentos en la línea de comandos.
-
+Autor: Javier Postigo Arévalo
+Fecha: /04/2023
 """
 import sys
 
